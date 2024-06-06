f6c9f86b7df8ce9c1caff481ce9bc50c209c370c
Sh_Jonizokov <Sh_Jonizokov@01-SH-JONIZOKOV>
2024-06-06 09:20:58 +0500

    Add project files.

---
+++ TestCICDInGitHubAction/Controllers/MathController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestCICDInGitHubAction.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MathController : ControllerBase
+{
+    [HttpGet]
+    public async Task<int> Calc()
+    {
+        return 5 + 8;
+    }
+}
